c7cc71b1616b085ce6e9ea48e2411207327e0b61
Mariet Stepanians <marietstepanyans@gmail.com>
2022-10-08 13:00:24 +0400

    Lesson 5 changed

--- Lesson_5/script.sh
+++ Lesson_5/script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-file_name=`whoami`-`date '+%Y-%m-%d-%H-%M-%S'`
-echo "`last`" > ${file_name}
-gzip ${file_name}
+set -xe
+
+dir_name=$1
+mkdir -p ${dir_name}
+file_name=${dir_name}/`whoami`-`date '+%Y-%m-%d-%H-%M-%S'`
+echo "`last`" > ${file_name} && gzip ${file_name}
