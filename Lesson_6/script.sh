eebd247b81138cacfdb7c9999a6907b21180955b
Mariet Stepanians <marietstepanyans@gmail.com>
2022-10-09 22:29:13 +0400

    Lesson 6

--- Lesson_6/script.sh
+++ Lesson_6/script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-set -xe
+#set -xe
+
+Help()
+{
+    echo "Missing inputs: name lastname age gender"
+}
 
 name=$1
 last_name=$2
 age=$3
 gender=$4
+
+if [[ -z "${name}" || -z "${last_name}" || -z "${age}" || -z "${gender}" ]]
+then
+      Help
+      exit 1
+fi
 
 echo "${name} ${last_name} ${age} ${gender}" >> users && cat users
