74b20fea09d3108c208d4096b808543b29ecb6d9
Mariet Stepanians <marietstepanyans@gmail.com>
2022-10-09 22:38:00 +0400

    Lesson 6

--- Lesson_6/script.sh
+++ Lesson_6/script.sh
@@ -4,7 +4,7 @@
 
 Help()
 {
-    echo "Missing inputs: name lastname age gender"
+    echo "Command syntax: ./script.sh <name> <last_name> <age> <gender>"
 }
 
 name=$1
@@ -12,9 +12,17 @@ last_name=$2
 age=$3
 gender=$4
 
+while getopts ":h" option; do
+   case $option in
+      h)
+         Help
+         exit;;
+   esac
+done
+
 if [[ -z "${name}" || -z "${last_name}" || -z "${age}" || -z "${gender}" ]]
 then
-      Help
+      echo "Missing one of the inputs: name lastname age gender"
       exit 1
 fi
 
